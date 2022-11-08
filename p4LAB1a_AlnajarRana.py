964b3be43a52e9f04f47ae1065b6ab90d9dd38aa
alnajarr9756 <113818598+alnajarr9756@users.noreply.github.com>
2022-11-07 22:37:21 -0500

    Add files via upload

---
+++ p4LAB1a_AlnajarRana.py
@@ -0,0 +1,17 @@
+import turtle
+ttl = turtle.Turtle()  
+for j in range(4):   
+  ttl.forward(150)  
+  ttl.left(90)
+
+triangle = turtle.Turtle()
+ 
+triangle.forward(100)
+ 
+triangle.left(120)
+triangle.forward(100)
+ 
+triangle.left(120)
+triangle.forward(100)
+ 
+turtle.done()
