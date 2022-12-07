32dba10e32cc88d17255b7f901b6400a68432335
alnajarr9756 <113818598+alnajarr9756@users.noreply.github.com>
2022-12-07 06:10:41 -0500

    Add files via upload

---
+++ P2LAB_Stats_AlnajarRana.py
@@ -0,0 +1,12 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+product = num1 * num2 * num3 * num4
+avgerage = (num1 + num2 + num3 + num4) /4
+
+print(f'{product:.0f} {avgerage:.0f}')
+print(f'{product:.3f} {avgerage:.3f}')
+
+''' Type your code here. '''
